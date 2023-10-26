8825abfb16c1fca4cc56e52ff7f8df87f3130011
Augusto Cuccione <cuccioneaugusto@gmail.com>
2023-10-26 01:23:35 -0300

    arreglos

---
+++ src/managers/productsManager.js
@@ -0,0 +1,21 @@
+import { productsModel} from "../db/models/products.model";
+
+class productsManager {
+    async findAll(){
+
+    }
+    async findById(){
+        
+    }
+    async createOne(){
+        
+    }
+    async updateOne(){
+        
+    }
+    async deleteOne(){
+        
+    }
+}
+
+export const productsManager = new productsManager()
